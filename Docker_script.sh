e8f67b5c0c852668aeeaa6b05f44e9bd776470de
jonerikrotnebo <jonrotnebo1@gmail.com>
2020-09-14 15:38:09 +0200

    fixing docker?

---
+++ Docker_script.sh
@@ -0,0 +1,5 @@
+ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  docker build . --tag pipelinetest --build-arg JAR_FILE=./target/demo-0.0.1-SNAPSHOT.jar
+  docker tag pipelinetest  jonrot/pipelinetest
+  docker push jonrot/pipelinetest
+  docker run -p 8080:8080 jonrot/pipelinetest
