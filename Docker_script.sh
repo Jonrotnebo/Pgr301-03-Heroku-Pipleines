f7be1a0c244eab81b02f55c7b2410fd63871eba5
jonerikrotnebo <jonrotnebo1@gmail.com>
2020-10-01 16:54:21 +0200

    fixing docker v6.

--- Docker_script.sh
+++ Docker_script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+##
+##From lesson 4 and 5.
+##
+
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build . --tag pipelinetest --build-arg JAR_FILE=./target/demo-0.0.1-SNAPSHOT.jar
 docker tag pipelinetest  jonrot/pipelinetest
--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,9 @@
+FROM maven:3.6-jdk-11 as builder
+WORKDIR /app
+COPY pom.xml .
+COPY src ./src
+RUN mvn package -DskipTests
+
 FROM adoptopenjdk/openjdk11:alpine-slim
 VOLUME /tmp
 ARG JAR_FILE
